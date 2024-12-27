f93c7f733be6bfaaaf938c4717b8c8e4b631a4f2
vinitsahoo1000 <vinitsahoo1000@gmail.com>
2024-12-27 23:24:53 +0530

    blog router updated

--- backend/dist/routes/blog.js
+++ backend/dist/routes/blog.js
@@ -1,13 +1,100 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.blogRouter = void 0;
+const chronicleshub_common_1 = require("@vinitsahoo1000/chronicleshub-common");
 const express_1 = __importDefault(require("express"));
+const client_1 = require("@prisma/client");
+const zod_1 = __importDefault(require("zod"));
+const multer_1 = __importDefault(require("multer"));
+const middleware_1 = require("../middleware");
+const cloudinary_1 = __importDefault(require("../config/cloudinary"));
+const prisma = new client_1.PrismaClient();
 exports.blogRouter = (0, express_1.default)();
-exports.blogRouter.get('/', (req, res) => {
-    res.send({
-        msg: "Blog Router!!"
-    });
-});
+const upload = (0, multer_1.default)({ dest: "uploads/" });
+exports.blogRouter.post("/post", middleware_1.authMiddleware, upload.single('image'), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const userId = req.userId;
+    try {
+        const payload = chronicleshub_common_1.createBlogInput.parse(req.body);
+        let imageUrl = null;
+        if (req.file) {
+            const result = yield cloudinary_1.default.uploader.upload(req.file.path, {
+                folder: "posts/",
+                resource_type: "image"
+            });
+            imageUrl = result.secure_url;
+        }
+        const blog = yield prisma.blog.create({
+            data: {
+                title: payload.title,
+                content: payload.content,
+                authorId: userId,
+                imageUrl: imageUrl
+            }
+        });
+        res.send({
+            msg: "Blog created successfully!!",
+            blog: blog
+        });
+    }
+    catch (e) {
+        if (e instanceof zod_1.default.ZodError) {
+            return res.status(400).json({
+                message: "Invalid input",
+                error: e.errors
+            });
+        }
+        console.error(e);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}));
+exports.blogRouter.put("/update/:id", middleware_1.authMiddleware, upload.single('image'), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const userId = req.userId;
+    const blogId = req.params.id;
+    try {
+        const payload = chronicleshub_common_1.updateBlogInput.parse(req.body);
+        let imageUrl = null;
+        if (req.file) {
+            const result = yield cloudinary_1.default.uploader.upload(req.file.path, {
+                folder: "posts/",
+                resource_type: "image"
+            });
+            imageUrl = result.secure_url;
+        }
+        const blog = yield prisma.blog.update({
+            where: {
+                id: blogId
+            },
+            data: {
+                title: payload.title,
+                content: payload.content,
+                imageUrl: imageUrl
+            }
+        });
+        res.send({
+            msg: "Blog updated successfully!!",
+            blog: blog
+        });
+    }
+    catch (e) {
+        if (e instanceof zod_1.default.ZodError) {
+            return res.status(400).json({
+                message: "Invalid input",
+                error: e.errors
+            });
+        }
+        console.error(e);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}));
--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,11 +1,97 @@
-import express from "express";
+import { createBlogInput, updateBlogInput } from "@vinitsahoo1000/chronicleshub-common";
+import express,{Request,Response} from "express";
+import { PrismaClient } from "@prisma/client";
+import z from "zod";
+import multer from "multer";
+import { authMiddleware } from "../middleware";
+import cloudinary from "../config/cloudinary";
+
+const prisma = new PrismaClient();
 
 export const blogRouter = express();
+const upload = multer({dest:"uploads/"});
+
+blogRouter.post("/post",authMiddleware,upload.single('image'),async(req:Request,res:Response):Promise<any>=>{
+    const userId = req.userId
+    try{
+        const payload = createBlogInput.parse(req.body)
+
+        let imageUrl = null;
+        if(req.file){
+            const result = await cloudinary.uploader.upload(req.file.path,{
+                folder:"posts/",
+                resource_type:"image"
+            })
+            imageUrl = result.secure_url;
+        }
+        const blog = await prisma.blog.create({
+            data:{
+                title:payload.title,
+                content:payload.content,
+                authorId: userId!,
+                imageUrl:imageUrl
+            }
+        })
+
+        res.send({
+            msg: "Blog created successfully!!",
+            blog: blog
+        })
+
+    }catch(e){
+        if(e instanceof z.ZodError){
+            return res.status(400).json({
+                message:"Invalid input",
+                error: e.errors
+            })
+        }
+
+        console.error(e);
+        res.status(500).json({message:"Internal server error"})
+    }
+});
 
 
-blogRouter.get('/',(req,res) =>{
-    res.send({
-        msg: "Blog Router!!"
-    })
+blogRouter.put("/update/:id",authMiddleware,upload.single('image'),async(req:Request,res:Response):Promise<any>=>{
+    const userId = req.userId
+    const blogId = req.params.id
+    try{
+        const payload = updateBlogInput.parse(req.body)
+
+        let imageUrl = null;
+        if(req.file){
+            const result = await cloudinary.uploader.upload(req.file.path,{
+                folder:"posts/",
+                resource_type:"image"
+            })
+            imageUrl = result.secure_url;
+        }
+
+        const blog = await prisma.blog.update({
+            where:{
+                id:blogId
+            },
+            data:{
+                title:payload.title,
+                content:payload.content,
+                imageUrl:imageUrl
+            }
+        })
+
+        res.send({
+            msg: "Blog updated successfully!!",
+            blog: blog
+        })
+
+    }catch(e){
+        if(e instanceof z.ZodError){
+            return res.status(400).json({
+                message:"Invalid input",
+                error: e.errors
+            })
+        }
+
+        console.error(e);
+        res.status(500).json({message:"Internal server error"})
+    }
 })
-
