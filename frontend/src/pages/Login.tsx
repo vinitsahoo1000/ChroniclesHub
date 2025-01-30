715ebbbad05bf7eaeb44f28b3be50d0e9dd9a73a
vinitsahoo1000 <vinitsahoo1000@gmail.com>
2025-01-30 13:12:51 +0530

    login and signup page redirection to blogs page added

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,15 @@
+import { Navigate } from "react-router-dom";
 import { AuthLogin } from "../components/AuthLogin";
 import { Quote } from "../components/Quote";
 
 
 export const Login = () => {
-    
+    const token = localStorage.getItem("token");
+
+    // If user is already logged in, redirect to /blogs
+    if (token) {
+    return <Navigate to="/blogs" replace />;
+    }
     return(
         <div className="grid grid-cols-1 lg:grid-cols-2 h-screen overflow-hidden">
             
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,8 +1,15 @@
+import { Navigate } from "react-router-dom";
 import { AuthSignup } from "../components/AuthSignup"
 import { Quote } from "../components/Quote"
 
 
 export const Signup = () => {
+    const token = localStorage.getItem("token");
+
+    // If user is already logged in, redirect to /blogs
+    if (token) {
+    return <Navigate to="/blogs" replace />;
+    }
     return(
         <div>
             <div className="grid grid-cols-1 lg:grid-cols-2 h-screen overflow-hidden">
