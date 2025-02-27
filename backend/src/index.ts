00507d6e74c20473fb2ce56f726e8adf36678c51
vinitsahoo1000 <vinitsahoo1000@gmail.com>
2025-02-27 14:59:43 +0530

    health checkpoint api added for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express,{Request,Response} from "express";
 import cors from "cors";
 import { mainRouter } from "./routes";
 const PORT = 3000;
@@ -10,6 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1",mainRouter);
+
+app.use('/health',async(req:Request,res:Response)=>{
+    console.log("server is running...")
+    res.send({
+        status:"OK"
+    })
+})
 
 app.listen(PORT, ()=>{
     console.log("The server is running on Port",PORT)
